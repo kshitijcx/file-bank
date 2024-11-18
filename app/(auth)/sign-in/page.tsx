95c4989f5adef4a374bd88063c7aa8d3f03e76ab
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-11-18 23:30:03 +0530

    sign-in, sign-up pages and AuthPage component created

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,6 @@
+import AuthPage from "@/components/AuthPage";
+
+const page = () => {
+  return <AuthPage type="sign-in"/>
+};
+export default page;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,6 @@
+import AuthPage from "@/components/AuthPage"
+
+const page = () => {
+  return <AuthPage type="sign-up" />
+}
+export default page
