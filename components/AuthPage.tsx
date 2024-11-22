9c6e78bed0fdc0608d6862eb06744faf08d5a3e2
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-11-22 11:27:33 +0530

    add sign-in, sign-up pages and create appwrite config

--- components/AuthPage.tsx
+++ components/AuthPage.tsx
@@ -6,54 +6,108 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+import { useState } from "react";
+import { LoaderCircle } from "lucide-react";
+import Link from "next/link";
 
 type FormType = "sign-in" | "sign-up";
 
+const formSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullname:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
 const AuthPage = ({ type }: { type: FormType }) => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const formSchemaObj = formSchema(type);
+
+  const form = useForm<z.infer<typeof formSchemaObj>>({
+    resolver: zodResolver(formSchemaObj),
     defaultValues: {
-      username: "",
+      fullname: "",
+      email: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (values: z.infer<typeof formSchemaObj>) => {
     console.log(values);
-  }
+  };
 
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="container mx-auto px-5 py-3 space-y-2">
-          <h1 className="">{type==="sign-in"?"Sign In":"Sign Up"}</h1>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="container w-1/3 h-screen mx-auto px-5 py-3 flex flex-col justify-center gap-2"
+        >
+          <h1 className="text-lg font-semibold">
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Full Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter full name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
           <FormField
             control={form.control}
-            name="username"
+            name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Email</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="Enter email" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <Button
+            type="submit"
+            className="flex justify-center gap-3 mt-2"
+            disabled={loading}
+          >
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+            {loading && <LoaderCircle size={24} className="animate-spin" />}
+          </Button>
+          {errorMsg && <p>{errorMsg}</p>}
+          {
+            <div className="flex justify-center gap-2 font-light text-sm mt-1">
+              <p className="text-gray-400">
+                {type === "sign-in"
+                  ? "Don't have an account?"
+                  : "Already have an account?"}
+              </p>
+              <Link
+                href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+                className="hover:underline"
+              >
+                {type === "sign-in" ? "Sign Up" : "Sign In"}
+              </Link>
+            </div>
+          }
         </form>
       </Form>
       {/* OTP Verification */}
