935acafea25a71a23c0a0c32fb5e72d43a63b74b
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-11-26 20:44:36 +0530

    add ActionDropdown component

---
+++ components/ActionDropdown.tsx
@@ -0,0 +1,117 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { actionsDropdownItems } from "@/constants";
+import { constructDownloadUrl } from "@/lib/utils";
+import { EllipsisVertical, Loader } from "lucide-react";
+import Link from "next/link";
+import { Models } from "node-appwrite";
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const ActionDropdown = ({ file }: { file: Models.Document }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [action, setAction] = useState<ActionType | null>(null);
+  const [name, setName] = useState(file.name);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const closeAllModals = () => {
+    setIsDropdownOpen(false)
+    setIsModalOpen(false)
+    setAction(null)
+    setName(file.name)
+    //setEmails([])
+  }
+
+  const handleActions = async () => {
+
+  }
+
+
+  const renderDialogContent = () => {
+    if (!action) return null;
+    const { value, label } = action;
+    return (
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{label}</DialogTitle>
+          {value === "rename" && (
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
+        </DialogHeader>
+        {["rename", "delete", "share"].includes(value) && (
+          <DialogFooter>
+            <Button onClick={closeAllModals} variant="secondary">Cancel</Button>
+            <Button onClick={handleActions}>Submit</Button>
+            {isLoading && <Loader size={20} className="animate-spin"/>}
+          </DialogFooter>
+        )}
+      </DialogContent>
+    );
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger>
+          <EllipsisVertical size={20} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="whitespace-nowrap overflow-hidden text-ellipsis">
+            {file.name}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {actionsDropdownItems.map((actionItem) => (
+            <DropdownMenuItem
+              key={actionItem.value}
+              onClick={() => {
+                setAction(actionItem);
+                if (
+                  ["rename", "share", "delete", "details"].includes(
+                    actionItem.value
+                  )
+                ) {
+                  setIsModalOpen(true);
+                }
+              }}
+            >
+              {actionItem.value === "download" ? (
+                <Link
+                  href={constructDownloadUrl(file.bucketFileId)}
+                  download={file.name}
+                >
+                  Download
+                </Link>
+              ) : (
+                <div>{actionItem.label}</div>
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {renderDialogContent()}
+    </Dialog>
+  );
+};
+export default ActionDropdown;
--- components/Card.tsx
+++ components/Card.tsx
@@ -3,27 +3,32 @@ import { Models } from "node-appwrite";
 import Thumbnail from "./Thumbnail";
 import { convertFileSize } from "@/lib/utils";
 import FormattedDateTime from "./FormattedDateTime";
+import ActionDropdown from "./ActionDropdown";
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
-    <Link
-      href={file.url}
-      target="_blank"
-      className="border-2 w-40 h-40 text-xs flex flex-col justify-between p-3 relative"
-    >
-      <div>
-        <Thumbnail type={file.type} extension={file.extension} size="40" />
+    <div className="relative">
+      <Link
+        href={file.url}
+        target="_blank"
+        className="border-2 w-40 h-40 text-xs flex flex-col justify-between p-3"
+      >
+        <div>
+          <Thumbnail type={file.type} extension={file.extension} size="40" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="whitespace-nowrap overflow-hidden text-ellipsis">
+            {file.name}
+          </p>
+          <p className="text-neutral-400">{convertFileSize(file.size)}</p>
+          <FormattedDateTime date={file.$createdAt} />
+          <p className="text-neutral-400">By: {file.owner.fullName}</p>
+        </div>
+      </Link>
+      <div className="absolute right-3 top-3">
+        <ActionDropdown file={file} />
       </div>
-      <div className="flex flex-col gap-1">
-        <p className="whitespace-nowrap overflow-hidden text-ellipsis">
-          {file.name}
-        </p>
-        <p className="text-neutral-400">{convertFileSize(file.size)}</p>
-        <FormattedDateTime date={file.$createdAt} />
-        <p className="text-neutral-400">By: {file.owner.fullName}</p>
-      </div>
-      <div className="absolute right-3 top-0">...</div>
-    </Link>
+    </div>
   );
 };
 export default Card;
